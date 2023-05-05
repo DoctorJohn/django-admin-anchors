15090260e566417919161e82b0fe4f6e12cf2af4
Jonathan Ehwald <github@ehwald.info>
2023-05-05 23:18:17 +0200

    Refactor the model field resolver

--- admin_anchors/utils.py
+++ admin_anchors/utils.py
@@ -1,10 +1,26 @@
 from typing import Dict, List, Optional
 
 from django.db import models
-from django.db.models.fields import Field
 from django.urls import reverse
 from django.utils.html import format_html
 from django.utils.http import urlencode
+from django.core.exceptions import ObjectDoesNotExist
+
+
+def resolve_instance_field_path(
+    instance: models.Model, field_path: List[str]
+) -> Optional[models.Model]:
+    if not field_path:
+        return instance
+
+    field_name = field_path.pop(0)
+
+    try:
+        next_instance = getattr(instance, field_name)
+    except ObjectDoesNotExist:
+        return None
+
+    return resolve_instance_field_path(next_instance, field_path)
 
 
 def create_admin_anchor(
@@ -19,22 +35,3 @@ def create_admin_anchor(
     if query:
         url += "?" + urlencode(query)
     return format_html("<a href='{}'>{}</a>", url, label)
-
-
-def get_selected_obj(instance: models.Model, selector: str) -> Optional[models.Model]:
-    current = instance
-    # The last part is the selected field
-    parts = selector.split(".")[0:-1]
-    for part in parts:
-        current = getattr(current, part, None)
-        if current is None:
-            return None
-    return current
-
-
-def get_selected_field(instance: models.Model, selector: str) -> Optional[Field]:
-    selection = get_selected_obj(instance, selector)
-    if selection is None:
-        return None
-    field_name = selector.split(".")[-1]
-    return selection._meta.get_field(field_name)
