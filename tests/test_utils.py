0f4aa188c9dd1d5c74821fa2e18aab356adc8df0
Jonathan Ehwald <github@ehwald.info>
2023-05-05 19:03:30 +0200

    Migrate to pytest test syntax

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,5 +1,5 @@
+import pytest
 from django.core.exceptions import FieldDoesNotExist
-from django.test import TestCase
 
 from admin_anchors.utils import (
     create_admin_anchor,
@@ -9,80 +9,90 @@ from admin_anchors.utils import (
 from tests.project.gaming.models import Player, Team
 
 
-class GetSelectedObjTestCase(TestCase):
-    def test_direct_related_object_selection(self):
-        player = Player.objects.create(name="John")
+@pytest.mark.django_db
+def test_get_selected_obj_direct_related_object_selection():
+    player = Player.objects.create(name="John")
 
-        obj = get_selected_obj(player, "profile")
-        self.assertEqual(obj, player)
-
-    def test_indirect_related_object_selection(self):
-        player = Player.objects.create(name="John")
-        team = Team.objects.create(captain=player)
-
-        obj = get_selected_obj(team, "captain.profile")
-        self.assertEqual(obj, player)
-
-    def test_invalid_selection_results_in_none(self):
-        player = Player.objects.create(name="John")
-        team = Team.objects.create(captain=player)
-
-        obj = get_selected_obj(team, "NON_EXISTING_RELATION.profile")
-        self.assertIsNone(obj)
+    obj = get_selected_obj(player, "profile")
+    assert obj == player
 
 
-class GetSelectedFieldTestCase(TestCase):
-    def test_raises_if_direct_field_selection_is_invalid(self):
-        player = Player.objects.create(name="John")
+@pytest.mark.django_db
+def test_get_selected_obj_indirect_related_object_selection():
+    player = Player.objects.create(name="John")
+    team = Team.objects.create(captain=player)
 
-        with self.assertRaises(FieldDoesNotExist):
-            get_selected_field(player, "NON_EXISTING_FIELD")
-
-    def test_raises_if_indirect_field_selection_is_invalid(self):
-        player = Player.objects.create(name="John")
-        team = Team.objects.create(captain=player)
-
-        with self.assertRaises(FieldDoesNotExist):
-            get_selected_field(team, "captain.NON_EXISTING_FIELD")
-
-    def test_returns_none_if_indirect_related_obj_selection_is_invalid(self):
-        player = Player.objects.create(name="John")
-        team = Team.objects.create(captain=player)
-
-        field = get_selected_field(team, "NON_EXISTING_RELATION.profile")
-        self.assertIsNone(field)
+    obj = get_selected_obj(team, "captain.profile")
+    assert obj == player
 
 
-class CreateAdminAnchorTestCase(TestCase):
-    def test_resolve_changelist_page(self):
-        link = create_admin_anchor(
-            page_name="changelist",
-            app_label="gaming",
-            model_name="player",
-            label="Players",
-        )
-        expectation = "<a href='/admin/gaming/player/'>Players</a>"
-        self.assertEqual(link, expectation)
+@pytest.mark.django_db
+def test_get_selected_obj_invalid_selection_results_in_none():
+    player = Player.objects.create(name="John")
+    team = Team.objects.create(captain=player)
 
-    def test_resolve_filtered_changelist_page(self):
-        link = create_admin_anchor(
-            page_name="changelist",
-            app_label="gaming",
-            model_name="player",
-            label="Players",
-            query={"teams__id": 1},
-        )
-        expectation = "<a href='/admin/gaming/player/?teams__id=1'>Players</a>"
-        self.assertEqual(link, expectation)
+    obj = get_selected_obj(team, "NON_EXISTING_RELATION.profile")
+    assert obj is None
 
-    def test_resolve_change_page(self):
-        player = Player.objects.create(name="John")
-        link = create_admin_anchor(
-            page_name="change",
-            app_label="gaming",
-            model_name="player",
-            label="Player",
-            args=[player.pk],
-        )
-        expectation = f"<a href='/admin/gaming/player/{player.pk}/change/'>Player</a>"
-        self.assertEqual(link, expectation)
+
+@pytest.mark.django_db
+def test_get_selected_field_raises_if_direct_field_selection_is_invalid():
+    player = Player.objects.create(name="John")
+
+    with pytest.raises(FieldDoesNotExist):
+        get_selected_field(player, "NON_EXISTING_FIELD")
+
+
+@pytest.mark.django_db
+def test_get_selected_field_raises_if_indirect_field_selection_is_invalid():
+    player = Player.objects.create(name="John")
+    team = Team.objects.create(captain=player)
+
+    with pytest.raises(FieldDoesNotExist):
+        get_selected_field(team, "captain.NON_EXISTING_FIELD")
+
+
+@pytest.mark.django_db
+def test_get_delected_field_returns_none_if_indirect_related_obj_selection_is_invalid():
+    player = Player.objects.create(name="John")
+    team = Team.objects.create(captain=player)
+
+    field = get_selected_field(team, "NON_EXISTING_RELATION.profile")
+    assert field is None
+
+
+def test_create_admin_anchor_resolves_changelist_page():
+    link = create_admin_anchor(
+        page_name="changelist",
+        app_label="gaming",
+        model_name="player",
+        label="Players",
+    )
+    expectation = "<a href='/admin/gaming/player/'>Players</a>"
+    assert link == expectation
+
+
+def test_create_admin_anchor_resolves_filtered_changelist_page():
+    link = create_admin_anchor(
+        page_name="changelist",
+        app_label="gaming",
+        model_name="player",
+        label="Players",
+        query={"teams__id": 1},
+    )
+    expectation = "<a href='/admin/gaming/player/?teams__id=1'>Players</a>"
+    assert link == expectation
+
+
+@pytest.mark.django_db
+def test_create_admin_anchor_resolves_change_page():
+    player = Player.objects.create(name="John")
+    link = create_admin_anchor(
+        page_name="change",
+        app_label="gaming",
+        model_name="player",
+        label="Player",
+        args=[player.pk],
+    )
+    expectation = f"<a href='/admin/gaming/player/{player.pk}/change/'>Player</a>"
+    assert link == expectation
