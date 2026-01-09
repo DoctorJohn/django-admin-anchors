726dc626bf5346b7aa1f6bcb6b453a567134d067
Jonathan Ehwald <github@ehwald.info>
2026-01-09 19:54:08 +0100

    Upgrade python syntax using pyupgrade

--- admin_anchors/utils.py
+++ admin_anchors/utils.py
@@ -1,4 +1,4 @@
-from typing import Any, Dict, List, Optional
+from typing import Any
 
 from django.core.exceptions import ObjectDoesNotExist
 from django.db import models
@@ -8,8 +8,8 @@ from django.utils.http import urlencode
 
 
 def resolve_instance_field_path(
-    instance: models.Model, field_path: List[str]
-) -> Optional[models.Model]:
+    instance: models.Model, field_path: list[str]
+) -> models.Model | None:
     if not field_path:
         return instance
 
@@ -27,7 +27,7 @@ def create_admin_anchor(
     app_label: str,
     model_name: str,
     label: str,
-    query: Dict[str, Any],
+    query: dict[str, Any],
 ) -> str:
     path = reverse(f"admin:{app_label}_{model_name}_changelist")
     query_string = urlencode(query)
